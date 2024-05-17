702a60c04215cd66932d8afe8487e347878140f9
김철우 <mooopjjang@gmail.com>
2024-05-17 17:29:06 +0900

    ing

--- todo/src/main.js
+++ todo/src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
+// import VueSidebarMenu from 'vue-sidebar-menu'
+// import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 
 
 Vue.config.productionTip = false
 Vue.prototype.$axios = axios
 Vue.prototype.$eventBus = new Vue()
 
+// Vue.use(VueSidebarMenu)
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
